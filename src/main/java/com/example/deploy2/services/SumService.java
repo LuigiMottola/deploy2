752b4ab439effe8abf4926b2187957054932beb0
LuigiMottola <sputnick427@gmail.com>
2024-04-23 09:54:03 +0200

    Termine esercizio

---
+++ src/main/java/com/example/deploy2/services/SumService.java
@@ -0,0 +1,13 @@
+package com.example.deploy2.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class SumService {
+    public Integer sumOfTwoRandomInt(){
+        Random generator = new Random();
+        return generator.nextInt() + generator.nextInt();
+    }
+}
